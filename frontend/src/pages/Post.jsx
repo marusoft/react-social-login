2b0ef02f084b02277bbc44e1da15b6ca0ff292bb
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-06-08 17:40:37 +0100

    complete frontend designd

--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -1,20 +1,21 @@
-
 import { useLocation } from "react-router";
-import {posts} from "../data";
+import { posts } from "../data";
 
 // const post = posts[2];
 
 const Post = () => {
   const location = useLocation();
-  console.log(location)
+  const path = location.pathname.split("/")[2];
+  const post = posts.find((p) => p.id.toString() === path);
+  console.log(location);
   return (
     <div className="post">
-      {/* <img src={post.img} alt="" className="postImg" />
+      <img src={post.img} alt="" className="postImg" />
       <h1 className="postTitle">{post.title}</h1>
       <p className="postDesc">{post.desc}</p>
-      <p className="postLongDesc">{post.longDesc}</p> */}
+      <p className="postLongDesc">{post.longDesc}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
